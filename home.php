49ab91691efd9797f4529d8987cc0eab402bde37
EnduringGuerila <timkirtland@gmail.com>
2014-12-10 03:13:41 -0600

    IEEE
    home.php > tntechieee.org
    
    index.html > enduringguerila.com/ieee2

---
+++ home.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+
+		<title>TTU IEEE</title>
+		<link rel="shortcut icon" href="favicon.ico" >
+		<link href="favicon.ico" rel="shortcut icon" type="image/x-icon" />
+
+		<link href="/Style/main.css" type="text/css" rel="stylesheet">
+		<link href="/Style/rss.css" type="text/css" rel="stylesheet">
+		<link href="/Style/boxes.css" type="text/css" rel="stylesheet">
+		<link href="/Style/blogpost.css" type="text/css" rel="stylesheet">
+
+		<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+		<meta http-equiv="Pragma" content="no-cache" />
+		<meta http-equiv="Expires" content="0" />		
+
+	</head>
+
+	
+
+	<body>
+
+		<div id="bodyWrapper">
+
+			<div id="header">
+
+				<img src="/Style/header.png" id="ni00">
+
+				<p class="tagline" id="ni01">Advancing Technology for Humanity</p>
+
+			</div>
+
+
+
+
+			<div id="navigation" class="sticker">
+
+				<div class="nav1 selected" onclick="navLink('')" id="ni02"><p id="ni03">Home</p></div>
+
+				<div class="nav2" onclick="navLink('About')" id="ni04"><p id="ni05">About Us</p></div>
+
+				<div class="nav3" onclick="navLink('Officers')" id="ni06"><p id="ni07">Officers</p></div>
+
+				<div class="nav4" onclick="navLink('Calendar')" id="ni08"><p id="ni09">Calendar</p></div>
+
+				<div class="nav5" onclick="navLink('Contact')" id="ni010"><p id="ni011">Contact Us</p></div>
+
+				<div class="nav6" onclick="navLink('Events')" id="ni0111"><p id="ni033">Events</p></div>
+
+				<p class="links" id="ni012">
+					<a href="https://www.facebook.com/IEEETTU" id="ni014">Facebook</a>
+					|
+					<a href="http://www.ieee.org" id="ni013">IEEE</a>
+					|
+					<a href="http://www.tntech.edu" id="ni014">TTU</a>
+					|
+					<a href="http://orgs.tntech.edu/hkn/" id="ni015">TTU HKN</a></p>
+
+			</div>
+
+			
+
+			<div id="slidingBoxes">
+
+				<div class="displayBox" name="noChange" id="ni012"><img src="techBanner.png" width="100%" height="100%" id="ni013"></div>
+
+				<div class="box1" name="techBanner.png" id="ni014"><p id="ni015">Tennessee Tech IEEE</p></div>
+
+				<div class="box2" name="Zaxbys.jpg" onclick="navLink('Events')" id="ni016"><p id="ni017">Internship Meeting Event</p></div>
+
+				<div class="box3" name="seconHardware.png" onclick="navLink('Secon')" id="ni018"><p id="ni019">2nd place for Hardware Team</p></div>
+
+				<!--<div class="box3"></div>
+
+				<div class="box4"></div>-->
+
+			</div>
+
+			
+
+			<div class="facefeed rounded">
+				<iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2FIEEETTU&amp;height=590&amp;colorscheme=dark&amp;show_faces=true&amp;header=false&amp;stream=true&amp;show_border=true" scrolling="no" allowTransparency="true">
+				</iframe>
+			</div>
+			
+
+
+
+			<div id="rssBox">
+				<div class="rssBox" id="ni020">
+				</div>
+
+				<div class="rssBox" id="ni021">
+				</div>
+			</div>
+
+
+			<style type="text/css">
+				div#rssincl-box-552998, div#rssincl-box-673152 { 
+					width: 100%; 				
+				}
+
+
+				div#rssincl-box-552998 div.rssincl-content table tr td, div#rssincl-box-673152 div.rssincl-content table tr td {
+					width:50%;
+					}
+
+				div.rssincl-last {
+					visibility: hidden;
+					}
+
+			</style>
+
+
+			<div id="blogpost">
+
+				<div class="article rounded secon15" id="ni0222">
+
+					<h2 id="ni0223" class="edit-txt">TTU IEEE Headed to Secon 2015</h2>
+
+					<p id="ni024" class="edit"> <img src="images/secon2015.jpg"> SoutheastCon is the annual IEEE Region 3 Technical, Professional, and Student Conference. <a href="http://www.ewh.ieee.org/reg/3/southeastcon2015/" id="ni025">For more.</a>  </p>
+
+				</div>
+
+				
+
+				<div class="article rounded" id="ni026">
+
+					<h2 id="ni027" class="edit-txt">Welcome to the site</h2>
+
+					<p id="ni028" class="edit">Thank you for visiting Tennessee Tech's IEEE chapter website.  We have redesigned it
+
+					   with simplicity in mind and hope that you can quickly find any information you need.
+
+					   Please let us know if you find any mistakes or have suggestions on how to make this
+
+					   site better.  This site is in a stage of continuous improvement which means we will
+
+					   be adding features and content over the next several months.  If you have content
+
+					   you think would add to this site such as pictures of IEEE events or an article you
+
+					   believe we should feature, email the <a href="mailto:tntechieee@gmail.com" id="ni029">
+
+					   Webmaster</a>.  Enjoy!</p>
+
+				</div>
+
+			</div>
+			
+
+
+
+			<div id="rssBox">
+				<div class="rssBox" id="ni020">
+					<?php readfile('http://output76.rssinclude.com/output?type=php&id=552998&hash=06eb98f0885cf73a3cb91e613f4cae5f')?>
+				</div>
+
+			</div>
+
+
+
+
+
+			<div id="footer">
+
+				<table id="footTable">
+
+					<tr>
+
+						<th id="ni030">Main Pages</th>
+
+						<th id="ni031">Featured Articles</th>
+
+						<th id="ni032">Links</th>
+
+					</tr>
+
+					<tr>
+
+						<td id="ni033"><a href="/" id="ni034">Home</a></td>
+
+						<td id="ni035"><a href="/Secon" id="ni036">SoutheastCon 2013</td>
+
+						<td id="ni037"><a href="http://www.tntech.edu" id="ni038">Tennessee Tech</a></td>
+
+					</tr>
+
+					<tr>
+
+						<td id="ni039"><a href="/About" id="ni040">About</a></td>
+
+						<td id="ni041"></td>
+
+						<td id="ni042"><a href="http://www.ieee.org" id="ni043">IEEE</a></td>
+
+					</tr>
+
+					<tr>
+
+						<td id="ni044"><a href="/Officers" id="ni045">Officers</a></td>
+
+						<td id="ni046"></td>
+
+						<td id="ni047"><a href="http://orgs.tntech.edu/hkn" id="ni048">TTU HKN</a></td>
+
+					</tr>
+
+					<tr>
+
+						<td id="ni049"><a href="/Calendar" id="ni050">Calendar</a></td>
+
+						<td id="ni051"></td>
+
+						<td id="ni052"></td>
+
+					</tr>
+
+					<tr>
+
+						<td id="ni053"><a href="/Contact" id="ni054">Contact Us</a></td>
+
+						<td id="ni055"></td>
+
+						<td id="ni056"></td>
+
+					</tr>
+
+				</table>
+
+			</div>
+
+		</div>
+
+
+		<!--<script src="http://code.jquery.com/jquery-latest.js"></script>
+
+		<script src="/Script/jquery-1.7.2.min.js" type="text/javascript"></script>-->
+		<script type="text/javascript" src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
+
+		<script src="/Script/main.js" type="text/javascript"></script>
+
+		<script src="/Script/jquery.sticky.js"></script>
+		<script>
+		  $(document).ready(function(){
+		    $(".sticker").sticky({topSpacing:0});
+		  });
+		</script> 
+
+	</body>
+
+</html>
